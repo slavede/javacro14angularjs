5789570275cdc5c6658fe543ca0eccc58a4651a1
Slaven Tomac <slaven@SlavenUbuntu1204Amsterdam.(none)>
2014-04-10 15:00:18 -0400

    Initial version of JavaCro14. Result of 'yo angular'

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('javaCro14App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
