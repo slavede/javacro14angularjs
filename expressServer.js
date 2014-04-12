cd13295a0729e7ea36f1cd70b4389d4b5463f73c
Slaven Tomac <slaven.tomac@gmail.com>
2014-04-12 09:06:27 -0400

    Moved links in layout (index.html). Added expressServer with some hardcoded data to return.

--- expressServer.js
+++ expressServer.js
@@ -2,7 +2,45 @@
 
 var express = require('express'),
 	args = process.argv.slice(2),
-	app = express();
+	app = express(),
+	participants = [
+	{
+		id : 0,
+		name : 'Tin',
+		surname : 'Tvrtkovic',
+		age : 29,
+		company : 'Amphinicy Technologies'
+	},
+	{
+		id : 1,
+		name : 'Bill',
+		surname : 'Gates',
+		age : 59,
+		company : 'Microsoft'
+	},
+	{
+		id : 2,
+		name : 'Mark',
+		surname : 'Zuckerberg',
+		age : 30,
+		company : 'Facebook'
+	},
+	{
+		id : 3,
+		name : 'Larry',
+		surname : 'Page',
+		age : 41,
+		company : 'Google'
+	},
+	{
+		id : 4,
+		name : 'Slaven',
+		surname : 'Tomac',
+		age : 26,
+		company : 'Amphinicy Technologies'
+	}
+];
+
 
 // console.log("Args", args);
 
@@ -12,18 +50,29 @@ var server = app.listen(args[1], args[2], function() {
 	console.log('Express server listening on host %s port %d', server.address().address, server.address().port);
 });
 
-app.get('/', function(req, res){
-	res.sendfile('app/index.html');
-});
+// app.get('/', function(req, res){
+// 	res.sendfile('app/index.html');
+// });
 
-app.get('/api/example', function(req, res) {
-	res.json(
-		[
-			{
-				name : 'Slaven',
-				surname : 'Tomac',
-				age : 26
+
+app.get('/api/1.0/participant/:id?', function(req, res) {
+	var paramId = req.param('id'),
+		response;
+
+	if (paramId === undefined) {
+		response = participants;
+	} else {
+		console.log('Getting participant with id', paramId);
+		participants.forEach(function(participant) {
+			console.log('Checking participant ', participant.id);
+			console.log(participant.id === parseInt(paramId));
+			if (participant.id === parseInt(paramId)) {
+				console.log('Found participant with id ', participant);
+				response = participant;
 			}
-		]
-		);
+		});
+	}
+
+	console.log('Will return', response);
+	return res.json(response);
 });
