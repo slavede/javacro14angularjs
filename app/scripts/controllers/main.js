0bdc55991e481b39110192f377e20db91faa58e7
Slaven Tomac <slaven.tomac@gmai.com>
2014-05-02 23:15:23 +0200

    Added example of AngularJS $interval and test for it

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,18 @@
 angular.module('javaCro14App')
-	.controller('MainCtrl', function ($scope) {
+	.controller('MainCtrl', ['$scope', '$interval', function ($scope, $interval) {
 		'use strict';
 		$scope.awesomeThings = [
 			'AngularJS',
 			'Karma',
 			'Jasmine'
 		];
-	});
+
+		$scope.messageInterval = $interval(
+			function() {
+				$scope.showMessage = !$scope.showMessage;
+			},
+			500,
+			2500);
+
+		$scope.showMessage = false;
+	}]);
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -4,18 +4,33 @@ describe('Controller: MainCtrl', function () {
 	beforeEach(module('javaCro14App'));
 
 	var MainCtrl,
-		scope;
+		scope,
+		$interval;
 
 	// Initialize the controller and a mock scope
-	beforeEach(inject(function ($controller, $rootScope) {
+	beforeEach(inject(function ($controller, $rootScope, _$interval_) {
 		// console.log("beforeEach");
 		scope = $rootScope.$new();
 		MainCtrl = $controller('MainCtrl', {
 			$scope: scope
 		});
+		$interval = _$interval_;
 	}));
 
-	it('should attach a list of awesomeThings to the scope', function () {
+	it ('should attach a list of awesomeThings to the scope', function () {
 		expect(scope.awesomeThings.length).toBe(3);
+	});
+
+	it ('should toggle showMessage successfully', function() {
+		expect(scope.showMessage).toBe(false, 'Initial value of showMessage should be false');
+
+		$interval.flush(500);
+		expect(scope.showMessage).toBe(true, 'After flushing 500 miliseconds showMessage should toggle');
+
+		$interval.flush(499);
+		expect(scope.showMessage).toBe(true, 'After flushing 499 miliseconds showMessage should remain the same');
+
+		$interval.flush(1);
+		expect(scope.showMessage).toBe(false, 'After flushing additional 1 milisecond showMessage should change (because of previous 499)');
 	});
 });
